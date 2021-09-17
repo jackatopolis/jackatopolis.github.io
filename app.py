d81e27526388dd23a47697fa8a7ce7cae57aa348
Jack Cohen <jackatopolis45@gmail.com>
2021-09-17 19:40:20 -0400

    Updated app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pymongo
-
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template,redirect,url_for,request,make_response
 
 conn = 'mongodb://localhost:27017'
 client = pymongo.MongoClient(conn)
@@ -17,17 +16,17 @@ def scrape():
     
     return data
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def home():
     from webscrape import scrape
-    data = scrape()
-    
-    db.info.insert_one(data)
-    out = list(db.info.find())
-    print(out)
-
-    # Return the template with the teams list passed in
-    return render_template('index.html', out=out)
+    if request.method == 'POST':
+        datafromjs = request.form['mydata']
+        data = scrape()
+        db.info.insert_one(data)
+        out = list(db.info.find())
+        resp = make_response(data)
+        return resp
+        return render_template('index.html', out=out)
 
 
 if __name__ == '__main__':
