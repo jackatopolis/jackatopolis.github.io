ea6766b762f459c7ea8d93544c4306b892beb898
Jack Cohen <jc@enpira.io>
2021-12-29 16:15:49 -0500

    Changes to index.html and app.py

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
 import numpy as np
 import pymongo
 from flask import Flask, jsonify, render_template,redirect,url_for,request,make_response
-
+from webscrape import scrape
 
 
 app = Flask(__name__)
 
 @app.route('/scrape')
 def scrape():
-    from webscrape import scrape
     data = scrape()
     return render_template('index.html', out=data)
 
 
 @app.route('/')
 def home():
-    from webscrape import scrape
-    #data = scrape()
-    return render_template('index.html')#, out=data)
+    data = scrape()
+    return render_template('index.html', out=data)
 
 @app.route('/update', methods=['POST'])
 def update():
-    from webscrape import scrape
     data = scrape()
     return jsonify({"jsdata": data})
 
