a2e686b74af539e8b3edabb76af4afaa88d10e76
Jack Cohen <jc@enpira.io>
2021-12-29 22:12:41 -0500

    fixed scraper

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 import numpy as np
 import pymongo
 from flask import Flask, jsonify, render_template,redirect,url_for,request,make_response
-import webscrape as ws
 
-#testData = scrape()
-data = ws.scrapeData()
 
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def home():
+    import webscrape as ws
+    data = ws.scrapeData()
     return render_template('index.html', out=data)
 
 
